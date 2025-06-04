662d2670e6492a240448ac3dd8ccf0da5becabc4
lainver <092018amir@gmail.com>
2025-06-04 15:22:58 +0700

    Update beranda.blade.php

--- resources/views/layouts/beranda.blade.php
+++ resources/views/layouts/beranda.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Header Website</title>
+  
+  <title>Sehat Harmoni Indonesia</title>
   <link rel="stylesheet" href="aset/css/cssnya.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
